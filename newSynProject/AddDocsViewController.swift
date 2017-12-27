41a2ad6aaac35f6a081961ca6b35ef67b31f1721
James Ye <jamye506@gmail.com>
2017-12-26 21:09:57 -0800

    add api patch

--- newSynProject/AddDocsViewController.swift
+++ newSynProject/AddDocsViewController.swift
@@ -32,66 +32,70 @@ class AddDocsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-            var bdayMonth :Int? = Int(dobMonthField.text!)
-            var bdayDay :Int? = Int(dobDayField.text!)
-            var bdayYear :Int? = Int(dobYearField.text!)
-            
-        let parameters: Parameters = [
-            "documents":
-                [["email": emailField.text,
-                  "phone_number": "901.111.1111",
-                  "ip":"123.123.123",
-                  "name":"Test User",
-                  "entity_type":"M",
-                  "entity_scope":"Arts & Entertainment",
-                  "day":2,
-                  "month":5,
-                  "year":1989,
-                  "address_street":"1 Market St.",
-                  "address_city":"SF",
-                  "address_subdivision":"CA",
-                  "address_postal_code":"94114",
-                  "address_country_code":"US"]]
-            
-        ]
-            
-            // Alamofire headers
-            let SPGATEWAY = ViewController().valueForAPIKey(named:"GATEWAY")
-            let SPUSER = ViewController().valueForAPIKey(named: "USER")
-            let SPUSERIP = ViewController().valueForAPIKey(named: "USER-IP")
-            
-            let userToken = self.userAuthToken!["oauth_key"] as! String
-            let oauthUser = userToken + SPUSER
-            
-            let headers: HTTPHeaders = [
-                "X-SP-GATEWAY": SPGATEWAY,
-                "X-SP-USER": oauthUser,
-                "X-SP-USER-IP": SPUSERIP
-            ]
-            
-            // Call to synaspefi API
-            let id = self.userRes!["_id"] as! String
-            
-            let apiString = "https://uat-api.synapsefi.com/v3.1/users/"
-            let searchId = id
-            
-            
-            let urlString : String = apiString + searchId
-            //        print(urlString)
-            
-            Alamofire.request(urlString, method: .patch, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON {
-                response in
-                debugPrint(response)
-                print("##############################")
-                print (response)
-                print("##############################")
-            }
     }
     
     var myDict = [String:String]()
     
+    func updateUser(){
+        var bdayMonth :Int? = Int(dobMonthField.text!)
+        var bdayDay :Int? = Int(dobDayField.text!)
+        var bdayYear :Int? = Int(dobYearField.text!)
+        
+        let parameters: Parameters = [
+            "documents":
+                [["email": emailField.text,
+                  "phone_number": phoneField.text,
+                  "ip":"123.123.123",
+                  "name":nameField.text,
+                  "entity_type":"M",
+                  "entity_scope":"Arts & Entertainment",
+                  "day":bdayDay,
+                  "month":bdayMonth,
+                  "year":bdayYear,
+                  "address_street":addressField.text,
+                  "address_city":cityField.text,
+                  "address_subdivision":cityField.text,
+                  "address_postal_code":zipField.text,
+                  "address_country_code":countryField.text]]
+            
+        ]
+        
+        // Alamofire headers
+        let SPGATEWAY = ViewController().valueForAPIKey(named:"GATEWAY")
+        let SPUSER = ViewController().valueForAPIKey(named: "USER")
+        let SPUSERIP = ViewController().valueForAPIKey(named: "USER-IP")
+        
+        let userToken = self.userAuthToken!["oauth_key"] as! String
+        let oauthUser = userToken + SPUSER
+        
+        let headers: HTTPHeaders = [
+            "X-SP-GATEWAY": SPGATEWAY,
+            "X-SP-USER": oauthUser,
+            "X-SP-USER-IP": SPUSERIP
+        ]
+        
+        // Call to synaspefi API
+        let id = self.userRes!["_id"] as! String
+        
+        let apiString = "https://uat-api.synapsefi.com/v3.1/users/"
+        let searchId = id
+        
+        
+        let urlString : String = apiString + searchId
+        //        print(urlString)
+        
+        Alamofire.request(urlString, method: .patch, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON {
+            response in
+            debugPrint(response)
+            print("##############################")
+            print (response)
+            print("##############################")
+        }
+    }
+    
+    
     @IBAction func submitButtonPressed(_ sender: UIButton) {
-
+        updateUser()
     }
 
 
